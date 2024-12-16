c382408d56dd10fe1ef77ed1b7dd1d00c7b77ccd
AhkamTasin <ahkamhussentasin2004@gmail.com>
2024-12-16 20:45:27 +0600

    code

--- easy_banking.c
+++ easy_banking.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<time.h>
 
 struct user{
     char phone[50];
@@ -16,6 +17,13 @@ struct user{
     char nomineenid[20];
     char nomineemobile[20];
     float balance;
+
+    // Loan related fields
+    float loan_amount;
+    float monthly_payment;
+    int loan_duration;
+    char last_payment_date[15]; // YYYY-MM-DD
+    float extra_charge;
 };
 
 int main(){
@@ -32,6 +40,8 @@ int main(){
     char cont='y';
     float amount;
     int oploan,loanch;
+    // char currentDate[50];
+    // char dueDate[50];
      
 
     printf("\n\033[1;32mWhat do you want to do?\033[0m\n"); 
@@ -239,78 +249,146 @@ int main(){
                             printf("| 2. Pay your monthly payment of Loan       |\n");
                             printf("|-------------------------------------------|\n");
                             printf("\nWhat is your choice?\t");
-                            printf("\n\nYour choice:\t");
-                            scanf("%d",&oploan);
+                            scanf("%d", &oploan);
 
-                            if(oploan==1){
+                            if (oploan == 1) {
                                 system("cls");
                                 printf("How much of a loan do you want?\n");
                                 printf("1. 1,00000 Taka - (You must have at least 10,000 Taka in your main balance)\n");
                                 printf("2. 2,00000 Taka - (You must have at least 50,000 Taka in your main balance)\n");
                                 printf("3. 5,00000 Taka - (You must have at least 1,00000 Taka in your main balance)\n");
-                                printf("\n\nCaution: [You must pay at least 10,000 Taka in every month.\n If you miss this 2%% charge of total loan amount will be added.\nIf you do advance deposit, no charge will be added]\n");
-
+                                printf("\nCaution: [You must pay at least 10,000 Taka monthly.\nIf you miss a payment, a 2%% charge of the total loan amount will be added.\nIf you pay in advance, no charge will be applied.]\n");
                                 printf("\n\nYour choice:\t");
-                                scanf("%d",&loanch);
-                                if(loanch==1){
-                                    fp=fopen(filename,"r");
-                                    fread(&usr,sizeof(struct user),1,fp);
-                                    if(usr.balance>=10000){
+                                scanf("%d", &loanch);
+
+                                float loan_amounts[] = {100000, 200000, 500000};
+                                float required_balances[] = {10000, 50000, 100000};
+
+                                if (loanch >= 1 && loanch <= 3) {
+                                    float selected_loan = loan_amounts[loanch - 1];
+                                    float required_balance = required_balances[loanch - 1];
+
+                                    if (usr.balance >= required_balance) {
+                                        // Update loan details
+                                        usr.loan_amount = selected_loan;
+                                        usr.monthly_payment = 10000.0;
+                                        usr.loan_duration = (int)(selected_loan / usr.monthly_payment);
+                                        usr.extra_charge = 0.0;
+
+                                        // Record the current date as last payment date
+                                        time_t t = time(NULL);
+                                        struct tm tm = *localtime(&t);
+                                        sprintf(usr.last_payment_date, "%04d-%02d-%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+
+                                        // Update balance
+                                        usr.balance += selected_loan;
+
+                                        // Save to file
+                                        fp = fopen(filename, "w");
+                                        fwrite(&usr, sizeof(struct user), 1, fp);
                                         fclose(fp);
-                                        fp=fopen(filename,"w");
-                                        usr.balance+=100000;
-                                        fwrite(&usr,sizeof(struct user),1,fp);
-                                        fclose(fp);
-                                        printf("\nLoan of 1,00000 Taka activated successfully...\n");
+
+                                        printf("\nLoan of %.2f Taka activated successfully!\n", selected_loan);
                                     }
-                                    else
-                                    {
-                                        printf("\nYou have no sufficient balance to take this Loan.\n");
-                                    }
+                                    else {
+                                        printf("\nYou have insufficient balance to take this loan.\n");
+                                        }
                                 }
-                                else if(loanch==2){
-                                    fp=fopen(filename,"r");
-                                    fread(&usr,sizeof(struct user),1,fp);
-                                    if(usr.balance>=50000){
-                                        fclose(fp);
-                                        fp=fopen(filename,"w");
-                                        usr.balance+=200000;
-                                        fwrite(&usr,sizeof(struct user),1,fp);
-                                        fclose(fp);
-                                        printf("\nLoan of 2,00000 Taka activated successfully...\n");
-                                    }
-                                    else
-                                    {
-                                        printf("\nYou have no sufficient balance to take this Loan.\n");
-                                    }
+                                else {
+                                    printf("\nInvalid loan choice.\n");
                                 }
-                                else if(loanch==3){
-                                    fp=fopen(filename,"r");
-                                    fread(&usr,sizeof(struct user),1,fp);
-                                    if(usr.balance>=100000){
-                                        fclose(fp);
-                                        fp=fopen(filename,"w");
-                                        usr.balance+=500000;
-                                        fwrite(&usr,sizeof(struct user),1,fp);
-                                        fclose(fp);
-                                        printf("\nLoan of 5,00000 Taka activated successfully...\n");
-                                    }
-                                    else
-                                    {
-                                        printf("\nYou have no sufficient balance to take this Loan.\n");
-                                    }
-                                }
-                                
                             }
-                        
+                            else if (oploan == 2) {
+                            system("cls");
+                            if (usr.loan_amount == 0) {
+                            printf("\nYou have no active loans.\n");
+                            } else {
+                                printf("\nYour current loan balance: Tk. %.2f", usr.loan_amount);
+                                printf("\nYour monthly payment: Tk. %.2f", usr.monthly_payment);
+                                printf("\nYour penalties so far: Tk. %.2f", usr.extra_charge); // Display penalties
+                                printf("\nEnter the amount you want to pay: \t");
+                                scanf("%f", &amount);
 
+                                // Get the current date
+                                char currentDate[15];
+                                time_t t = time(NULL);
+                                struct tm tm = *localtime(&t);
+                                sprintf(currentDate, "%04d-%02d-%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
 
+                                // Manually parse the last payment date
+                                int lastYear, lastMonth, lastDay;
+                                sscanf(usr.last_payment_date, "%d-%d-%d", &lastYear, &lastMonth, &lastDay);
 
+                                // Get the current date parts
+                                int currentYear = tm.tm_year + 1900;
+                                int currentMonth = tm.tm_mon + 1;
+                                int currentDay = tm.tm_mday;
 
+                                // Calculate the difference in days
+                                int days_diff = 0;
 
+                                // If the last payment date is older than the current date, calculate the difference
+                                if (currentYear > lastYear || (currentYear == lastYear && currentMonth > lastMonth) || (currentYear == lastYear && currentMonth == lastMonth && currentDay > lastDay)) {
+                                    // Convert the last payment date and current date to days since a fixed point (e.g., 01/01/1900)
+                                    int totalLastDays = lastYear * 365 + lastMonth * 30 + lastDay;
+                                    int totalCurrentDays = currentYear * 365 + currentMonth * 30 + currentDay;
 
+                                    // Calculate the number of days between the last payment date and current date
+                                    days_diff = totalCurrentDays - totalLastDays;
+                                }
 
+                                // If there are overdue payments
+                                if (days_diff > 30) {
+                                    int missed_months = days_diff / 30; // Calculate the number of missed months
+                                    usr.extra_charge = usr.loan_amount * 0.02 * missed_months; // Apply 2% penalty for missed months
+                                    usr.loan_amount += usr.extra_charge; // Add the penalty to the loan amount
+                                    printf("\nYou missed %d month(s) of payments. An extra penalty of Tk. %.2f has been added.\n", missed_months, usr.extra_charge);
+                                }
 
+                                // If the payment covers the loan amount
+                                if (amount >= usr.loan_amount) {
+                                    usr.loan_amount = 0;
+                                    printf("\nLoan fully repaid! Congratulations!\n");
+                                } else {
+                                    usr.loan_amount -= amount;
+                                    printf("\nPayment of Tk. %.2f accepted. Remaining loan balance: Tk. %.2f\n", amount, usr.loan_amount);
+                                }
+
+                                // Calculate how many months are skipped if the user pays more than the required monthly amount
+                                int monthsSkipped = 0;
+                                if (amount > usr.monthly_payment) {
+                                    monthsSkipped = (int)(amount / usr.monthly_payment); // Determine how many months the payment covers
+                                    if (monthsSkipped > 1) {
+                                        printf("\nYou have paid for the next %d month(s).\n", monthsSkipped - 1); // Display the skipped months
+                                    }
+                                }
+
+                                // Calculate the next payment date (add skipped months)
+                                int nextYear = currentYear;
+                                int nextMonth = currentMonth + monthsSkipped; // Add skipped months to the current month
+        
+                                // Handle month rollover
+                                while (nextMonth > 12) {
+                                    nextMonth -= 12;
+                                    nextYear++;
+                                }
+
+                                // Set the next payment date
+                                char nextPaymentDate[15];
+                                sprintf(nextPaymentDate, "%04d-%02d-%02d", nextYear, nextMonth, currentDay);
+
+                                // Display the next payment date
+                                printf("\nYour next payment is due on: %s\n", nextPaymentDate);
+
+                                // Update last payment date
+                                strcpy(usr.last_payment_date, currentDate);
+
+                                // Save to file
+                                fp = fopen(filename, "w");
+                                fwrite(&usr, sizeof(struct user), 1, fp);
+                                fclose(fp);
+                            }
+                        }
                     }
 
                     printf("\n\nDo you want to continue the transaction? [y/n] ");
